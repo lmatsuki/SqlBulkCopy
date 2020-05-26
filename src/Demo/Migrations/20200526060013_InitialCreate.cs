b3f31c9958b21b9894e6e869885d21c667e32072
Leon Matsuki <lmatsuki87@gmail.com>
2020-05-25 23:21:58 -0700

    Include migrations and SQL script

---
+++ src/Demo/Migrations/20200526060013_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Demo.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    NickName = table.Column<string>(nullable: true),
+                    Hp = table.Column<int>(nullable: false),
+                    Mp = table.Column<int>(nullable: false),
+                    Attack = table.Column<int>(nullable: false),
+                    Defense = table.Column<int>(nullable: false),
+                    MagicAttack = table.Column<int>(nullable: false),
+                    MagicDefense = table.Column<int>(nullable: false),
+                    Level = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Cost = table.Column<int>(nullable: false),
+                    Cooldown = table.Column<decimal>(nullable: false),
+                    Level = table.Column<int>(nullable: false),
+                    IsLearned = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Skills_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_PlayerId",
+                table: "Skills",
+                column: "PlayerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
---
+++ src/Demo/migration-script.sql
@@ -0,0 +1,50 @@
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Players] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NULL,
+    [NickName] nvarchar(max) NULL,
+    [Hp] int NOT NULL,
+    [Mp] int NOT NULL,
+    [Attack] int NOT NULL,
+    [Defense] int NOT NULL,
+    [MagicAttack] int NOT NULL,
+    [MagicDefense] int NOT NULL,
+    [Level] int NOT NULL,
+    CONSTRAINT [PK_Players] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [Skills] (
+    [Id] int NOT NULL IDENTITY,
+    [PlayerId] int NOT NULL,
+    [Name] nvarchar(max) NULL,
+    [Cost] int NOT NULL,
+    [Cooldown] decimal(18,2) NOT NULL,
+    [Level] int NOT NULL,
+    [IsLearned] bit NOT NULL,
+    CONSTRAINT [PK_Skills] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Skills_Players_PlayerId] FOREIGN KEY ([PlayerId]) REFERENCES [Players] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [IX_Skills_PlayerId] ON [Skills] ([PlayerId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20200526060013_InitialCreate', N'3.1.4');
+
+GO
+
