c5c4f6b8ced638e02c74cbfafb89465b05f21ed9
Leon Matsuki <lmatsuki87@gmail.com>
2020-06-07 23:50:55 -0700

    Place shared method in extension method

---
+++ src/Demo/Infrastructure/Extensions/InsertExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Demo.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Demo.Infrastructure.Extensions
+{
+    public static class InsertExtensions
+    {
+        public static IList<Player> GetPlayers(int playerCount)
+        {
+            IList<Player> players = new List<Player>();
+
+            for (int i = 0; i < playerCount; i++)
+            {
+                int id = i + 1;
+                players.Add(new Player
+                {
+                    Name = $"Player {id}",
+                    Skills = new List<Skill>
+                    {
+                        new Skill(id, "Bash", id),
+                        new Skill(id, "Increase Attack", id),
+                        new Skill(id, "Heal", id)
+                    },
+                    CorrelationId = id
+                });
+            }
+
+            return players;
+        }
+    }
+}
